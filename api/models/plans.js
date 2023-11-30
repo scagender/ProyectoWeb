b5f4cb5ffc9ff901b2d80eecd34e77f9e967fe21
Vicente Del Fierro <vicente.delfierro@uc.cl>
2023-11-30 01:46:02 -0300

    hotfix

--- api/models/plans.js
+++ api/models/plans.js
@@ -19,10 +19,6 @@ module.exports = (sequelize, DataTypes) => {
     Plan.belongsTo(models.User, {
       foreignKey: 'user_id'
     })
-    Plan.belongsToMany(models.Course, {
-      through: 'plan_courses',
-      foreignKey: 'plan_id'
-    })
   }
 
   return Plan
